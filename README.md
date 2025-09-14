63ce3484f1a71443f1baeb1b08b1736aaf5eac5f
Saumya Subham Mishra <109909356+SaumyaSubham@users.noreply.github.com>
2025-09-14 13:29:30 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# GURUKUL UNIVERSITY
+
+
+ 
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p>© 2025 Saumya Subham Mishra. All rights reserved.</p>
+      <ul>
+        <li><a href="https://www.youtube.com/channel/UCeiIZFDhydGrnCOVZVnwEIg/about?edit_links=1&disable_polymer=1#c4-primary-header-contents" target='_blank'>Terms of Services</a></li>
+        <li><a href="https://www.youtube.com/channel/UCeiIZFDhydGrnCOVZVnwEIg/about?edit_links=1&disable_polymer=1#c4-primary-header-contents" target='_blank'>Privacy Policy</a></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Footer
